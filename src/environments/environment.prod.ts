e8bd8e467defa0a0f7f2f77dc1694f317c71dea1
allesonsales <allesonsales@gmail.com>
2026-03-12 11:01:21 -0300

    Fix: Botão iniciar viagem

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  apiUrl: 'https://vania-be-production.up.railway.app',
-  // apiUrl: 'http://localhost:3000',
+  // apiUrl: 'https://vania-be-production.up.railway.app',
+  apiUrl: 'http://localhost:3000',
 };
