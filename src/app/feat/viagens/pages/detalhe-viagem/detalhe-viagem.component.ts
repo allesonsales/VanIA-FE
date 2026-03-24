f55ef62c86f3bf8d07e95fa01a9054f6dbea797e
allesonsales <allesonsales@gmail.com>
2026-03-24 12:52:42 -0300

    Fix: tabela viagens

--- src/app/feat/viagens/pages/detalhe-viagem/detalhe-viagem.component.ts
+++ src/app/feat/viagens/pages/detalhe-viagem/detalhe-viagem.component.ts
@@ -5,31 +5,41 @@ import { ViagemService } from '../../../../service/viagem.service';
 import { Viagens } from '../../../../../types/Viagens';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
+import { FlashMessageService } from '../../../../service/flash-message.service';
+import { LoadingComponent } from '../../../../shared/components/loading/loading.component';
 
 @Component({
   selector: 'app-detalhe-viagem',
-  imports: [HeaderComponent, CommonModule, IonicModule],
+  imports: [HeaderComponent, CommonModule, IonicModule, LoadingComponent],
   templateUrl: './detalhe-viagem.component.html',
   styleUrl: './detalhe-viagem.component.css',
 })
 export class DetalheViagemComponent implements OnInit {
   viagem: Viagens | null = null;
+  loading: boolean = false;
+
   constructor(
     private route: ActivatedRoute,
     private viagemService: ViagemService,
+    private flashMessage: FlashMessageService,
   ) {}
 
   ngOnInit(): void {
     document.title = 'VanIA | Detalhe Viagem';
 
+    this.loading = true;
+
     const id = Number(this.route.snapshot.paramMap.get('id'));
 
     this.viagemService.consultarViagem(id).subscribe({
       next: (res: any) => {
+        this.loading = false;
         this.viagem = res;
         console.log(res);
       },
       error: (err) => {
+        this.loading = false;
+        this.flashMessage.show(err.error.message, err.error.status);
         console.log(err);
       },
     });
--- src/app/feat/viagens/pages/viagens/viagens.component.ts
+++ src/app/feat/viagens/pages/viagens/viagens.component.ts
@@ -3,26 +3,32 @@ import { ViagemService } from '../../../../service/viagem.service';
 import { HeaderComponent } from '../../../../shared/components/header/header.component';
 import { Viagens } from '../../../../../types/Viagens';
 import { TabelaComponent } from '../../components/tabela/tabela.component';
+import { LoadingComponent } from '../../../../shared/components/loading/loading.component';
 
 @Component({
   selector: 'app-viagens',
-  imports: [HeaderComponent, TabelaComponent],
+  imports: [HeaderComponent, TabelaComponent, LoadingComponent],
   templateUrl: './viagens.component.html',
   styleUrl: './viagens.component.css',
 })
 export class ViagensComponent implements OnInit {
   constructor(private viagemService: ViagemService) {}
+  loading: boolean = false;
   viagens: Viagens[] = [];
 
   ngOnInit(): void {
     document.title = 'VanIA | Viagens';
 
+    this.loading = true;
+
     this.viagemService.consultarViagens().subscribe({
       next: (res: any) => {
+        this.loading = false;
         this.viagens = res;
         console.log(res);
       },
       error: (err) => {
+        this.loading = false;
         console.log(err);
       },
     });
